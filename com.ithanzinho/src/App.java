64e21b2f107115de3913ddec1a7111198c9a700d
Íthan P. Amaral <ithanamaral@gmail.com>
2024-10-31 07:53:20 -0300

    Subindo Arquivos

---
+++ com.ithanzinho/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Papiro Magico");
+        System.out.print("Louvado seja Deus");
+        
+    }
+}
